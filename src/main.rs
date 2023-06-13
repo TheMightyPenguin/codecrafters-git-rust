f0b4c4be52958ff5c271465dd4fc02fcd743bf91
Victor Tortolero <victormtortolero@gmail.com>
2023-06-12 22:13:55 -0600

    add cat-file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-#[allow(unused_imports)]
+use flate2::read::ZlibDecoder;
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use std::io::prelude::*;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    match args[1].as_str() {
+        "init" => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+            println!("Initialized git directory")
+        }
+
+        "cat-file" => {
+            // TODO: implement arg parsing lel
+            let blob_sha = args[3].clone();
+            let dir_name = &blob_sha[0..2];
+            let file_name = &blob_sha[2..];
+            let contents = fs::read(format!(".git/objects/{}/{}", dir_name, file_name)).unwrap();
+
+            let mut z = ZlibDecoder::new(&contents[..]);
+            let mut contents = String::new();
+            z.read_to_string(&mut contents).unwrap();
+
+            print!("{}", contents);
+        }
+
+        _ => {
+            println!("unknown command: {}", args[1])
+        }
     }
 }
